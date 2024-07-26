e3ae2b886d48db8429d295a6584667cb67d85b50
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-26 12:09:05 +0200

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Home",
+
+  setup() {
+    const posts = ref([
+      { title: "Welcome to the blog", body: "Lorem ipsum", id: 1 },
+      { title: "Top 5 CSS Tips", body: "lorem ipsum", id: 2 },
+    ]);
+
+    return { posts };
+  },
+};
+</script>
+
+<style></style>
